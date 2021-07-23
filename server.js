50ea52d603c8347a333e4d9bdd2d0386372ae3b3
adwait <adwaitsupekar@gmail.com>
2021-07-23 15:16:06 +0530

    gitignore added

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ import express from 'express';
 const path = require('path');
 
 const app = express();
-app.use('/', express.static)
+app.use('/', express.static(path.join(_dirname, 'static')));
+
+
+app.listen(9000, () => {
+    console.log('server up at 8000');
+})
