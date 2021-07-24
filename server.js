2ce389f2b2ffb59a354f3bff5f600fdde11cc3d7
adwait <adwaitsupekar@gmail.com>
2021-07-24 15:24:35 +0530

    post request added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.json());
 
+app.post('/api/register', async (req, res) => {
+    console.log(req.body);
+})
 
 app.listen(8000, () => {
     console.log('server up at 8000');
