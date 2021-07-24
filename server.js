c9a17b33c4e0d740de766d11e8a7452c3296cd87
adwait <adwaitsupekar@gmail.com>
2021-07-24 12:49:40 +0530

    static issue solved

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use('/', static(join(__dirname, 'static')));
+app.use('/', express.static(path.join(__dirname, 'static')));
 
 
-app.listen(9000, () => {
+app.listen(8000, () => {
     console.log('server up at 8000');
 })
