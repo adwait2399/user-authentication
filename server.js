76027dfb144db2378fffc2f6ab328d01f28dc9b3
adwait <adwaitsupekar@gmail.com>
2021-07-25 15:36:55 +0530

    :tada: mongoose added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-//const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/user-authentication');
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
@@ -8,8 +10,9 @@ app.use(express.json());
 
 app.post('/api/register', async (req, res) => {
     console.log(req.body);
-})
+    res.json({ status: 'ok' })
+});
 
 app.listen(8000, () => {
     console.log('server up at 8000');
-})
+});
