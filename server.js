a8ac24b2a7998da60a6b6ee9797248158347fbab
adwait <adwaitsupekar@gmail.com>
2021-07-26 14:45:16 +0530

    :card_file_box: user database created

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const User = require('./model/user');
 
-mongoose.connect('mongodb://localhost:27017/user-authentication');
+mongoose.connect('process.env.MONGO_URI',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
