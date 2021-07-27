926b4586c628d5b84dff99a4633915e2a3018b6c
adwait <adwaitsupekar@gmail.com>
2021-07-27 15:11:58 +0530

    :sparkles: user registration complete

--- server.js
+++ server.js
@@ -19,11 +19,39 @@ app.use(express.json());
 app.post('/api/register', async (req, res) => {
     console.log(req.body);
     
-    const { username, password } = req.body;
+    const { username, password: plainTextPassword } = req.body;
 
-    const Password = await bcrypt.hash(password, 10);
+    if(!username || typeof username !== 'string' ){
+        return res.json({ status: 'error', error: 'Invalid username' });
+    }
     
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' });
+    }
+
+    if (plainTextPassword.length < 5){
+        return res.json({ status: 'error', error:'password too small should be atleast 6 characters' });
+    }
+
+    const password = await bcrypt.hash( plainTextPassword, 10);
     
+    try{
+        const response = await User.create({
+            username,
+            password
+        });
+        
+        console.log('User created successfully: ',response);
+    
+    }catch(error){
+        
+        if(error.code === 11000){
+            return res.json({ status: 'error' , error:'Username already taken'});
+        }
+        throw error 
+        console.log(error.message);
+        return res.json({ status: 'error'})
+    }
 
     res.json({ status: 'ok' })
 });
