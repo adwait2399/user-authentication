6d37f52813c50c995bdcc5f688dccc0e95cabadb
adwait <adwaitsupekar@gmail.com>
2021-07-27 16:04:14 +0530

    :sparkles:login page & jwt token created

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const User = require('./model/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "fnij()&_*shfguirdhf44^$(@Qgujihdsuiouhu";
 
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
@@ -16,6 +18,32 @@ const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
 app.use(express.json());
 
+app.post('/api/login', async (req, res) => {
+    
+    const{ username, password } = req.body;
+
+    const user = await User.findOne({ username }).lean();
+
+    if(!user){
+        return res.json({ status: 'error', error: 'Invalid username or password'});
+    }
+    
+    if( await bcrypt.compare(password, user.password)){
+        
+        const token = jwt.sign({
+             id: user._id, 
+             username: user.username
+            },
+            JWT_SECRET
+        )
+
+        return res.json({ status: 'ok', data: token });
+    }
+    
+    res.json({ status: 'error', error: 'Invalid username or password' });
+})
+
+// REGISTRATION  PAGE  API
 app.post('/api/register', async (req, res) => {
     console.log(req.body);
     
