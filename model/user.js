1442ecd3679ffe601218d82a953f6dc4c5d08a4d
adwait <adwaitsupekar@gmail.com>
2021-07-30 20:48:33 +0530

    :lipstick: register page styling done

--- model/user.js
+++ model/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true},   
-},
-{ collection: 'users' }
+    password: { type: String, required: true },
+  },
+  { collection: 'users' }
 );
 
 const model = mongoose.model('userSchema', userSchema);
